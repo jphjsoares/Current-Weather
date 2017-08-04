cdcb19b80d3966d3c9c363e3a6af96ab74b91b35
José Soares <josephjsoares@aecastrodaire.com>
2017-08-04 21:51:22 +0100

    Add files via upload
    Here we make the conversion possible and we also add freecodecamp's icons. I don't really like them but oh well... Better than nothing

--- _scripts/geolocation.js
+++ _scripts/geolocation.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     var api = "https://fcc-weather-api.glitch.me/api/current?";
 
     if (navigator.geolocation)  {
@@ -7,22 +8,80 @@ $(document).ready(function() {
 
             //GET THE COORDS
             var lat = "lat=" + position.coords.latitude;
+
             var lon = "lon=" + position.coords.longitude;
+
             getWeather(lat, lon);
+
             })
 
         } else {
+
             alert("Geolocation is not supported by this browser");
         }
 
+
+        //GET WEATHER AND OTHER CHEESY STUFF
         function getWeather(lat, lon){
+
             var urlString = api + lat + "&" + lon;
+
             $.getJSON(urlString, function(data){
+
+                var degrees = data.main.temp;
+
+                var unit = "c";
+
+                var temperature = {
+                    "degrees": degrees,
+                    "unit": unit
+                };
+
+                var icon = data.weather[0].icon;
+
+                $("#icon").attr("src", icon);
+
                 $("#city-country").html(data.name + ", " + data.sys.country);
+
                 $("#description").html(data.weather[0].main);
-                $("#temp").html(data.main.temp + " °C");
+
+                $("#temp").html(temperature.degrees + " °C");
+
+
                 console.log(data);
+
+
+                function convert(){
+
+                    if(temperature.unit == "c"){
+
+                        temperature.degrees+=273.15;
+
+                        $("#temp").html(temperature.degrees + " °F");
+
+                        temperature.unit = "f";
+                    }
+
+                    else if (temperature.unit == "f") {
+
+                        temperature.degrees-=273.15;
+
+                        $("#temp").html(temperature.degrees + " °C");
+
+                        temperature.unit = "c";
+
+                    }
+
+                }
+
+                $("#toggle").click(function(){
+
+                    convert();
+
+                });
+
             });
+
         }
 
 }); //end of document function
