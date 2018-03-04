0aa2407b6271efeaa4a3c2c0080518922b76071a
José Soares <josephjsoares@aecastrodaire.com>
2018-03-04 14:32:24 +0000

    Accuracy improvement and icon fix
    In this commit, the icon was displayed and the geolocation accuracy was improved. TODO's were added to the project.

--- _scripts/geolocation.js
+++ _scripts/geolocation.js
@@ -1,87 +1,110 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    var api = "https://fcc-weather-api.glitch.me/api/current?";
-
-    if (navigator.geolocation)  {
-
-        navigator.geolocation.getCurrentPosition(function(position){
-
-            //GET THE COORDS
-            var lat = "lat=" + position.coords.latitude;
-
-            var lon = "lon=" + position.coords.longitude;
-
-            getWeather(lat, lon);
-
-            })
-
-        } else {
-
-            alert("Geolocation is not supported by this browser");
-        }
-
-
-        //GET WEATHER AND OTHER CHEESY STUFF
-        function getWeather(lat, lon){
-
-            var urlString = api + lat + "&" + lon;
-
-            $.getJSON(urlString, function(data){
-
-                var degrees = data.main.temp;
-
-                var unit = "c";
-
-                var temperature = {
-                    "degrees": degrees,
-                    "unit": unit
+        var api = "https://fcc-weather-api.glitch.me/api/current?";
+       
+        //Get geolocation
+        if("geolocation" in navigator) {
+                //Geolocation options
+                var options = {
+                    enableHighAccuracy: true,
+                    timeout: 20000,
+                    maximumAge: 0
                 };
 
-                var icon = data.weather[0].icon;
+                //If it succeeded to get current postion
+                function success(pos) {
+                    var crd = pos.coords;
+                    var lat = "lat=" + crd.latitude;
+                    var lon = "lon=" + crd.longitude;
+                    getWeather(lat, lon);
+                    /*
+                    console.log('Your current position is:');
+                    console.log(`Latitude : ${crd.latitude}`);
+                    console.log(`Longitude: ${crd.longitude}`);
+                    console.log(`More or less ${crd.accuracy} meters.`);
+                    */
+                };
+                
+                //If it failed to get current position
+                function error(err) {
+                    alert(`ERROR(${err.code}): ${err.message}`);
+                };
 
-                $("#icon").attr("src", icon);
+                navigator.geolocation.getCurrentPosition(success, error, options);
 
-                $("#city-country").html(data.name + ", " + data.sys.country);
-
-                $("#description").html(data.weather[0].main);
-
-                $("#temp").html(temperature.degrees + " °C");
+            } else {
+                alert("Something went wrong!\nWe couldn't find your location!");
+            }
 
 
-                console.log(data);
+            //GET WEATHER AND OTHER CHEESY STUFF
+            function getWeather(lat, lon){
+
+                var urlString = api + lat + "&" + lon;
+
+                $.getJSON(urlString, function(data){
+
+                    var degrees = data.main.temp;
+
+                    var unit = "c";
+
+                    var temperature = {
+                        "degrees": degrees,
+                        "unit": unit
+                    };
+
+                    var icon = data.weather[0].icon;
+
+                    $("#icon").attr("src", icon);
+
+                    $("#city-country").html(data.name + ", " + data.sys.country);
+
+                    $("#description").html(data.weather[0].main);
+
+                    $("#temp").html(temperature.degrees + " °C");
 
 
-                function convert(){
+                    console.log(data);
 
-                    if(temperature.unit == "c"){
 
-                        temperature.degrees+=273.15;
+                    function convert(){
 
-                        $("#temp").html(temperature.degrees + " °F");
+                        //Convert to fahrenheit
+                        if(temperature.unit == "c"){
 
-                        temperature.unit = "f";
+                            temperature.degrees+=273.15;
+
+                            $("#temp").html(temperature.degrees + " °F");
+
+                            temperature.unit = "f";
+                        }
+
+                        //Convert to Celsius
+                        else if (temperature.unit == "f") {
+
+                            temperature.degrees-=273.15;
+
+                            $("#temp").html(temperature.degrees + " °C");
+
+                            temperature.unit = "c";
+
+                        }
+
                     }
 
-                    else if (temperature.unit == "f") {
+                    $("#toggle").click(function(){
 
-                        temperature.degrees-=273.15;
+                        convert();
 
-                        $("#temp").html(temperature.degrees + " °C");
-
-                        temperature.unit = "c";
-
-                    }
-
-                }
-
-                $("#toggle").click(function(){
-
-                    convert();
+                    });
 
                 });
 
-            });
+            }
 
-        }
+    }); //end of document function
 
-}); //end of document function
+/* 
+    TODO:
+        Increase geolocation accuracy
+*/
